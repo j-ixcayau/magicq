86d9aae5e8025377d2f79621a6453f0b2d2e77d6
Jonathan Ixcayau <jonynija@gmail.com>
2024-10-05 00:49:25 -0600

    Add vscode config

--- lib/features/main_map/main_map_page.dart
+++ lib/features/main_map/main_map_page.dart
@@ -2,20 +2,29 @@ import 'package:flutter/material.dart';
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MainMapPage extends StatelessWidget {
+class MainMapPage extends StatefulWidget {
   const MainMapPage({super.key});
+
+  @override
+  State<MainMapPage> createState() => _MainMapPageState();
+}
+
+class _MainMapPageState extends State<MainMapPage> {
+  final initialLocation = const LatLng(14.613303, -90.535587);
+  late GoogleMapController controller;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
         mapType: MapType.normal,
-        initialCameraPosition: const CameraPosition(
-          target: LatLng(14.613303, -90.535587),
+        initialCameraPosition: CameraPosition(
+          target: initialLocation,
           zoom: 14,
         ),
         onMapCreated: (GoogleMapController controller) {
-          /* _controller.complete(controller); */
+          controller = controller;
+          setState(() {});
         },
       ),
     );
